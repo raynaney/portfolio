0fd305c9a1dd897b0d46ec6429eeaf03eb705085
Rayna  Ney <rlney@ucdavis.edu>
2023-10-30 16:03:33 +0100

    Update Model.stories.js

--- src/components/Model/Model.stories.js
+++ src/components/Model/Model.stories.js
@@ -1,4 +1,3 @@
-import phoneTexture2Large from 'assets/gamestack-list-large.jpg';
 import phoneTexture2Placeholder from 'assets/gamestack-list-placeholder.jpg';
 import phoneTexture2 from 'assets/gamestack-list.jpg';
 import phoneTextureLarge from 'assets/gamestack-login-large.jpg';
@@ -17,50 +16,6 @@ export default {
 
 const modelStyle = { position: 'absolute', top: 0, right: 0, bottom: 0, left: 0 };
 
-export const Phone = () => (
-  <StoryContainer padding={0}>
-    <Model
-      style={modelStyle}
-      cameraPosition={{ x: 0, y: 0, z: 11.5 }}
-      alt="Phone models"
-      models={[
-        {
-          ...deviceModels.phone,
-          position: { x: -0.6, y: 0.8, z: 0.1 },
-          texture: {
-            srcSet: [phoneTexture, phoneTextureLarge],
-            placeholder: phoneTexturePlaceholder,
-          },
-        },
-        {
-          ...deviceModels.phone,
-          position: { x: 0.6, y: -0.8, z: 0.4 },
-          texture: {
-            srcSet: [phoneTexture2, phoneTexture2Large],
-            placeholder: phoneTexture2Placeholder,
-          },
-        },
-      ]}
-    />
-  </StoryContainer>
-);
+export const Phone = () => <StoryContainer padding={0}></StoryContainer>;
 
-export const Laptop = () => (
-  <StoryContainer padding={0}>
-    <Model
-      style={modelStyle}
-      cameraPosition={{ x: 0, y: 0, z: 8 }}
-      alt="Laptop model"
-      models={[
-        {
-          ...deviceModels.laptop,
-          position: { x: 0, y: 0, z: 0 },
-          texture: {
-            srcSet: [laptopTexture, laptopTextureLarge],
-            placeholder: laptopTexturePlaceholder,
-          },
-        },
-      ]}
-    />
-  </StoryContainer>
-);
+export const Laptop = () => <StoryContainer padding={0}></StoryContainer>;
