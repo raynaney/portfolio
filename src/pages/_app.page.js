c4d62865ffb2d9cc538caeaeb1fcda588100af50
Rayna  Ney <rlney@ucdavis.edu>
2023-10-30 16:03:41 +0100

    Update _app.page.js

--- src/pages/_app.page.js
+++ src/pages/_app.page.js
@@ -19,8 +19,8 @@ export const AppContext = createContext({});
 
 const repoPrompt = `
 __  __  __
-\u005C \u005C \u005C \u005C \u005C\u2215\n \u005C \u005C\u2215\u005C \u005C\n  \u005C\u2215  \u005C\u2215
-\n\nTaking a peek huh? Check out the source code: https://github.com/HamishMW/portfolio
+
+\n\nTaking a peek huh? Check out the source code: https://github.com/raynaney/portfolio
 `;
 
 const App = ({ Component, pageProps }) => {
